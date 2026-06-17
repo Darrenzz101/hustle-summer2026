97355ba76dff52045e24dfa04df72386440c2782
Darren Lam <darrenlam@Darrens-MacBook-Air.local>
2026-06-17 10:29:39 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,12 @@
+full_name = "Darren Lam"
+nickname = "D"
+age = 17
+#have you used Python before?
+used_python_before = False
+hobbies = ["badminton", "gaming", "binge watching"]
+faves = {"movie" : ["Spiderman", "Batman", "Superman", "The Flash"]}
+print(full_name, nickname, used_python_before, age, hobbies, faves)
+
+skills = ("Leadership", "teamwork", "communication") # tuple: ordered, cannot change 
+languages_spoken = {"English", "Cantonese"} # set: no duplicates 
+print(skills, languages_spoken)
